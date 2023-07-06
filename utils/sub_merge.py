648434e28bcdb1a44b0dbd9df76ac2946f656bbc
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-07-06 21:30:43 +0800

    更新 sub_merge.py

--- utils/sub_merge.py
+++ utils/sub_merge.py
@@ -91,7 +91,7 @@ class merge():
              print('完成!\n')
              f.write(data)
     # 读取并解码 Base64 编码的节点信息
-    with open(merge_path, 'rb', encoding='utf-8') as file:
+    with open(merge_path, 'r', encoding='utf-8') as file:
         encoded_content = file.read().strip()
     # 进行 Base64 解码和解析
     decoded_content = base64_decode(encoded_content)
