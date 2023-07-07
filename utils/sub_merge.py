0aab538db7c0570737a3037d44c30b9fb012f15b
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-07-07 08:22:34 +0800

    更新 sub_merge.py

--- utils/sub_merge.py
+++ utils/sub_merge.py
@@ -45,6 +45,7 @@ class merge():
         content_set = set()
         for url in url_list:
             content = convert(url['url'], 'url', {'keep_encode': True, 'raw_format': True, 'escape_special_chars': True})
+            content = content.replace("!<str> ", "")
             if content:
                 content_set.update(content.splitlines())
                 print(f'Writing content of {url["remarks"]} to {url["id"]:0>2d}.txt')
