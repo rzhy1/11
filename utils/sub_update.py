8744395a47fd98d99cdead7042642c901b55abe4
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-07-08 17:45:39 +0800

    更新 sub_update.py

--- utils/sub_update.py
+++ utils/sub_update.py
@@ -118,7 +118,6 @@ class update():
             return current_url
 
     def find_link(self, id, current_url):
-    
         if id == 22:
             url_update = 'https://bitbucket.org/huwo1/proxy_nodes/src/main/'
 
@@ -126,15 +125,16 @@ class update():
                 try:
                     resp = requests.get(url_update, timeout=5)
                     raw_content = resp.text
-                    pattern = re.compile(r'https://bitbucket.org/huwo1/proxy_nodes/raw/.*?/proxy.md')
-                    new_url = re.findall(pattern, raw_content)[0]
+
+                    pattern = re.compile(r'https://bitbucket.org/huwo1/proxy_nodes/raw/.*?/proxy\.md')
+                    new_url = re.search(pattern, raw_content).group()
                     return new_url
                 except Exception:
                     return current_url
             else:
                 return current_url
-        else:
-                return current_url        
+        return current_url
+    
         if id == 33:
             url_update = 'https://v2cross.com/archives/1884'
 
